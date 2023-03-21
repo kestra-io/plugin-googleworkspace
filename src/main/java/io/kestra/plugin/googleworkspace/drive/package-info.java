25ad190718443760ac11c823c30d5f215f895157
Loïc Mathieu <loikeseke@gmail.com>
2023-03-21 14:31:47 +0100

    feat: plugin categories and icons

---
+++ src/main/java/io/kestra/plugin/googleworkspace/drive/package-info.java
@@ -0,0 +1,7 @@
+@PluginSubGroup(
+    description = "This sub-group of plugins contains tasks for using Google Drive.",
+    categories = { PluginSubGroup.PluginCategory.STORAGE, PluginSubGroup.PluginCategory.CLOUD }
+)
+package io.kestra.plugin.googleworkspace.drive;
+
+import io.kestra.core.models.annotations.PluginSubGroup;
---
+++ src/main/java/io/kestra/plugin/googleworkspace/sheets/package-info.java
@@ -0,0 +1,7 @@
+@PluginSubGroup(
+    description = "This sub-group of plugins contains tasks for using Google Sheet.",
+    categories = { PluginSubGroup.PluginCategory.MISC, PluginSubGroup.PluginCategory.CLOUD }
+)
+package io.kestra.plugin.googleworkspace.sheets;
+
+import io.kestra.core.models.annotations.PluginSubGroup;
