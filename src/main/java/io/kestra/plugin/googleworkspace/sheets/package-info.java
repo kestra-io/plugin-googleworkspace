1cc3b92cf4cd3bdfb06c6410f2557732413c26d8
Loïc Mathieu <loikeseke@gmail.com>
2024-04-19 17:41:51 +0200

    fix: remove PluginCategory.MISC

--- src/main/java/io/kestra/plugin/googleworkspace/sheets/package-info.java
+++ src/main/java/io/kestra/plugin/googleworkspace/sheets/package-info.java
@@ -1,6 +1,6 @@
 @PluginSubGroup(
     description = "This sub-group of plugins contains tasks for using Google Sheet.",
-    categories = { PluginSubGroup.PluginCategory.MISC, PluginSubGroup.PluginCategory.CLOUD }
+    categories = { PluginSubGroup.PluginCategory.CLOUD }
 )
 package io.kestra.plugin.googleworkspace.sheets;
 
