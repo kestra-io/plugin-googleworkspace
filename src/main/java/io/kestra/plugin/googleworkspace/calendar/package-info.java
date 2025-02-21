d3847a07a68a14db1c14c67b168178d39b9c71dd
brian.mulier <bmmulier@hotmail.fr>
2025-02-21 14:28:59 +0100

    fix: add package-info.java for calendar package
    (cherry picked from commit ca02854b48301900f40f834665dc0b4d23ab4c49)

---
+++ src/main/java/io/kestra/plugin/googleworkspace/calendar/package-info.java
@@ -0,0 +1,7 @@
+@PluginSubGroup(
+    description = "This sub-group of plugins contains tasks for using Google Calendar.",
+    categories = { PluginSubGroup.PluginCategory.CLOUD }
+)
+package io.kestra.plugin.googleworkspace.calendar;
+
+import io.kestra.core.models.annotations.PluginSubGroup;
