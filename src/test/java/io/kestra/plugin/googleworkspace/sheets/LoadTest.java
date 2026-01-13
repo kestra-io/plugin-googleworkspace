3028ff98e13faf2ee2fbfb9552355f67da2525c4
Malay Dewangan <malaydewangan310@gmail.com>
2026-01-13 21:23:15 +0530

    tests(): attempt to fix flaky tests

--- src/test/java/io/kestra/plugin/googleworkspace/sheets/LoadTest.java
+++ src/test/java/io/kestra/plugin/googleworkspace/sheets/LoadTest.java
@@ -13,6 +13,8 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.condition.DisabledIf;
+import org.junit.jupiter.api.parallel.Execution;
+import org.junit.jupiter.api.parallel.ExecutionMode;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -32,10 +34,9 @@ import static org.hamcrest.Matchers.*;
     value = "isServiceAccountNotExists",
     disabledReason = "Disabled for CI/CD"
 )
+@Execution(ExecutionMode.SAME_THREAD)
 class LoadTest {
     private static final AtomicInteger ROW_CURSOR = new AtomicInteger(1);
-
-    private static final String TEST_SHEET = "Sheet1";
 
     @Inject
     private RunContextFactory runContextFactory;
