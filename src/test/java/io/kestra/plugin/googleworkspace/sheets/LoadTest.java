4eab2244362fbf088db24a88b0cad1ca4de8af1d
Malay Dewangan <malaydewangan310@gmail.com>
2026-01-13 15:42:04 +0530

    tests(): attempt to fix flaky tests

--- src/test/java/io/kestra/plugin/googleworkspace/sheets/LoadTest.java
+++ src/test/java/io/kestra/plugin/googleworkspace/sheets/LoadTest.java
@@ -155,11 +155,11 @@ class LoadTest {
 
          var run = RetryUtils.<Load.Output, Exception>of().
             runRetryIf(isRetryableExternalFailure, () -> {
-                synchronized (GOOGLE_API_LOCK) {
-                    return task.run(
-                        runContext
-                    );
-                }
+                    synchronized (GOOGLE_API_LOCK) {
+                        return task.run(
+                            runContext
+                        );
+                    }
                 }
             );
 
